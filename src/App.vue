059a5c76f263e53a4598fc9aeb4d06a56e244722
Thomas Draijer <tomdraijer@MacBook-Pro-van-Thomas-4.local>
2021-08-06 17:04:39 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Hoeveel rookt Mars? </h1>
+
+<div id="counterApp">
+  <div class="sentenceBeforeStart"></div>
+  <div class="sentenceAfterStart">
+    <h2> {{firstName}} heeft {{count}} {{peukie}} gerookt</h2>
+    </div>
+  <div class="messageDiv">
+    <h3>{{message}}</h3>
+  </div>
+  <div id="buttons">
+      <button class="btnLeft" v-on:click="incrementCounter">Peukie erbij</button>
+      <button class="btnRight" v-on:click="resetCounter">Gestopt met roken</button>
+  </div>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      count: 0,
+      firstName: 'Mars',
+      message: 'Je gaat toch niet klikken he? ',
+      peukie: 'peukies'
+    }
+  },
+  methods: {
+    incrementCounter: function () {
+      this.count += 1
+      if (this.count <= 10) {
+        this.message = 'Ben je weer begonnen?'
+        if (this.count === 1) {
+          this.peukie = 'peukie'
+        } else {
+          this.peukie = 'peukies'
+        }
+      } else if (this.count <= 20) {
+        this.message = 'Weet je het zeker?'
+      } else if (this.count <= 30) {
+        this.message = 'Dit gaat niet goed..'
+      } else if (this.count <= 40) {
+        this.message = 'Je zit al op 30 gulden..'
+      } else {
+        this.message = 'Dit wordt niks meer lul'
+      }
+    },
+    resetCounter: function () {
+      this.count = 0
+      this.firstName = 'Mars'
+      this.message = 'En weer gestopt. Erg mooi. '
+      this.peukie = 'peukies'
+    }
   }
 }
 </script>
@@ -22,5 +66,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#buttons {
+  margin: 0px;
+  padding: 0px;
+}
+
+.btnLeft {
+  margin-right: 5px;
+}
+
+.btnRight {
+  margin-left: 5px;
 }
 </style>
